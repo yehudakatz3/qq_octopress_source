dc4243ff144b2bedf9e5a2e61c6501ceb32514d2
yehudakatz3 <yehuda.katz1@aol.com>
2013-07-06 09:02:58 +0300

    init

---
+++ config.rb
@@ -0,0 +1,18 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/qq/"
+http_images_path = "/qq/images"
+http_fonts_path = "/qq/fonts"
+css_dir = "public/qq/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
